a18de32adcfec1a5047f1d0a49b6b7f50588b3c2
jocker18291 <dawidans714@gmail.com>
2026-05-13 01:51:23 +0200

    Added ancillary chunk decoder

--- png_decoder.py
+++ png_decoder.py
@@ -5,7 +5,7 @@ import struct
 def decode(path_to_file):
     with open(path_to_file, 'rb') as file:
         file.read(8)  # First 8 bytes are the PNG signature.
-
+        color_type = None
         while True:
             length_bytes = file.read(4)
             if not length_bytes:
@@ -25,6 +25,7 @@ def decode(path_to_file):
                     print(f"    Width: {w} px, Height: {h} px")
                     print(f"    Byte Depth: {depth}, Color type: {c_type}")
                     print(f"    Compression: {comp}, Filter: {filt}, Interlace: {interl}")
+                    color_type = c_type
 
                 elif chunk_type == b'PLTE':
                     color_quantity = length // 3
@@ -38,3 +39,23 @@ def decode(path_to_file):
                 elif chunk_type == b'IEND':
                     print("    It means the valid end of png file")
                     break
+            
+            elif chunk_type in [b'tRNS', b'gAMA', b'cHRM']:
+                block_name = chunk_type.decode('ascii')
+                print(f"\n[+] Ancillary chunk found: {block_name} (Size: {length} bytes)")
+
+                if chunk_type == b'tRNS':
+                    if color_type == 1:
+                        print(f"   Transparency for indexed-color: {length} bytes")
+                    elif color_type == 2:
+                        print(f"   Transparency for true-color: {length} bytes")
+                    elif color_type == 3:
+                        print(f"   Transparency for grayscale: {length} bytes")
+                
+                elif chunk_type == b'gAMA':
+                    gamma_value = struct.unpack('>I', chunk_data)[0] / 100000
+                    print(f"   Gamma value: {gamma_value}")
+                
+                elif chunk_type == b'cHRM':
+                    white_x, white_y, red_x, red_y, green_x, green_y, blue_x, blue_y = struct.unpack('>IIIIIIII', chunk_data)
+                    print(f"   Chromaticity: White({white_x}, {white_y}), Red({red_x}, {red_y}), Green({green_x}, {green_y}), Blue({blue_x}, {blue_y})")
